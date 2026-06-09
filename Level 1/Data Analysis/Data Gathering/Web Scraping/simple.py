58b23026a41ef57eb8a4ebb975926fd6c6ae3865
Precipitation-Rain <rajvardhanshewale1200@gmail.com>
2026-06-09 21:44:21 +0530

    Add Web scraping files

---
+++ Level 1/Data Analysis/Data Gathering/Web Scraping/simple.py
@@ -0,0 +1,6 @@
+# By what % value increased or decrease
+initial_Value = int(input("Enter Initial Value :"))
+final_Value = int(input("Enter Final Value :"))
+
+what_percent_incDec = ((final_Value - initial_Value) / initial_Value) * 100
+print(what_percent_incDec , "%")
